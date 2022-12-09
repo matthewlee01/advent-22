0f7a98c7f8ab71bb8b936567c672b437453a247c
matthew lee <laughew@gmail.com>
2022-12-08 17:53:33 -0800

    day6-pt2

---
+++ 6/pt2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("input:\n")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := 14; i < len(text); i++ {
+		buf := text[i-14 : i]
+		unique := true
+		fmt.Println(buf)
+	out:
+		for j, c1 := range buf[:13] {
+			for _, c2 := range buf[j+1:] {
+				if c1 == c2 {
+					fmt.Println(string(c1))
+					unique = false
+					i += j
+					break out
+				}
+			}
+		}
+		if unique {
+			fmt.Println(i)
+			break
+		}
+	}
+}
